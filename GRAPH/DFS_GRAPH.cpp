94285a95b1c5fc950cfd7a2f0f768b1d9e1bc8ce
Karan <kr893781@gmail.com>
2026-08-14 23:29:31 +0530

    STUDIED TILL PUT OPERATION

---
+++ GRAPH/DFS_GRAPH.cpp
@@ -0,0 +1,6 @@
+//DFS IN GRAPH
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    
+}
--- STUDENT_MANAGMENT_API/main.py
+++ STUDENT_MANAGMENT_API/main.py
@@ -35,17 +35,216 @@
 # def students():
 #     return {"message":"All Students"}
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
+# app=FastAPI()
+# @app.get("/")
+# def home():
+#     return{
+#         "message":"Hello Karan you have stared learning fastAPI"
+#     }
+
+# @app.get("/teacher")
+# def teacher():
+#     return{
+#         "name":"Mr. Sharma",
+#         "subject":"Mathematics"
+#     }
+
+
+
+
+
+# from fastapi import FastAPI
+# app=FastAPI()
+# @app.get("/")
+# def home():
+#     return{
+#         "message":"Student Managment API"
+#     }
+
+# @app.get("/students")
+# def get_students():
+#     return{
+#         "students":["Rahul","Priya","Amit"]
+#     }
+
+# @app.post("/students")
+# def create_students():
+#     return{
+#         "message":"Student Created Sucessfully"
+#     }
+
+# @app.put("\students")
+# def update_student():
+#     return{
+#         "message":"Students Updated Sucessfully"
+#     }
+
+# @app.delete("/students")
+# def delete_students():
+#     return{
+#         "message":"Student Deleted Sucessfully"
+#     }
+
+
+
+
+#NOW WE ARE LEARNING THE REQUEST BODY WHICH MEANS HOW ACTUALL DATA IS TRANSFERRED FROM CLIENT TO THE BACKEND
+# from fastapi import FastAPI
+# from pydantic import BaseModel 
+# app=FastAPI()
+# class Student(BaseModel):
+#     name:str
+#     age:int
+#     email:str
+# @app.get("/")
+# def home():
+#     return{
+#         "message":"Student Managment API"
+#     }
+
+# @app.post("/students")
+# def create_student(student:Student):
+#     return student
+
+
+#CODE TO DEMONSTRATE PATH PARAMETERS
+# @app.get("/students/{student_id}")
+# def get_student(student_id:int):
+#     return{
+#         "student_id":student_id
+#     }
+
+# CODE TO DEMONSTRATE QUERY PARAMETER
+# @app.get("/students")
+# def get_students(department:str):
+#     return{
+#         "department":department
+#     }
+
+
+#LETS COMBINE ALL TOGHTHER PATH
+#PATH PARAMETER, QUERY PARAMETER, RESPONsE BODY
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# app=FastAPI()
+
+# class Student(BaseModel):
+#     name:str
+#     age:int
+#     email:str
+
+# @app.get("/students/{student_id}")
+# def get_student(student_id:int):
+#     return{
+#         "student_id":student_id
+#     }
+
+# @app.get("/students")
+# def get_students(
+#     department:str|None=None,
+#     page:int=1
+#     ):
+#     return{
+#         "department":department,
+#         "page":page
+#     }
+
+# @app.post("/students")
+# def create_student(student:Student):
+#     return student
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# app=FastAPI()
+
+# class Student(BaseModel):
+#     name:str
+#     age:int
+#     email:str
+#     pocket_money:int 
+
+# @app.get("/students/{student_id}")
+# def get_student(student_id:int):
+#     return{
+#         "student_id":student_id
+#     }
+
+# @app.get("/students")
+# def get_students(department:str|None=None,page:int=1):
+#     return{
+#         "department":department,
+#         "page":page
+#     }
+
+# @app.post("/students")
+# def create_student(student:Student):
+#     return student
+
+
+#MODULE-5
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+
 app=FastAPI()
+
+class Student(BaseModel):
+    name:str
+    age:int
+    email:str
+    department:str
+
+students=[]
+
+next_id=1
+
 @app.get("/")
 def home():
     return{
-        "message":"Hello Karan you have stared learning fastAPI"
+        "message":"Student Managment API"
     }
 
-@app.get("/teacher")
-def teacher():
-    return{
-        "name":"Mr. Sharma",
-        "subject":"Mathematics"
+@app.post("/students")
+def create_student(student:Student):
+    global next_id
+
+    new_student={
+        "id":next_id,
+        "name":student.name,
+        "age":student.age,
+        "email":student.email,
+        "department":student.department
     }
+
+    students.append(new_student)
+    next_id+=1
+
+    return new_student
+
+@app.get("/students")
+def get_students():
+    return students
+
+@app.get("/students/{student_id}")
+def get_student(student_id:int):
+    for student in students:
+        if student["id"]==student_id:
+            return student
+
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+@app.put("/students/{student_id}")
+def update_student(student_id:int,student:Student):
+    for existing_student in students:
+        if existing_student["id"]==student_id:
+            existing_student["name"]=student.name
+            existing_student["age"]=student.age
+            existing_student["department"]=student.email
+            return existing_student
+        raise HTTPException(
+            status_code=404,
+            details="Student not found"
+        )
